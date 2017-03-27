c68ec5040981dedcf8e326b2250571eb479090cc
zhenglinghua <zhenglinghua@zhenglinghuadeMacBook-Pro.local>
2017-03-27 16:31:26 +0800

    第一版1.12

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import _ from 'lodash';
+
 function component () {
   var element = document.createElement('div');
 
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['你好','哈哈哈'], ' ');
+  element.innerHTML = "你好，第一版本"
 
   return element;
 }
